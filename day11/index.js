73ec9b77cea026734b418a117db2060c4d08cf8d
Chris Miller <chrismilleruk@gmail.com>
2019-12-14 18:26:44 +0000

    Day 11, part 2

--- day11/index.js
+++ day11/index.js
@@ -1,32 +1,57 @@
 const { loadIntcodeFile } = require('../lib/loadIntcode');
-const { paintingRobot } = require('./paintingRobot');
-const { renderPanels } = require('./render');
+const { paintingRobot, paintingRobotEventGenerator, initPanels } = require('./paintingRobot');
+const { renderAllPanels, calculatePlotExtents, preparePlotArea, plotPanels } = require('./render');
 const readline = require('readline');
 const chalk = require('chalk');
 
 const filename = __dirname + '/input.txt';
 
+const configs = [
+  {
+    title: 'start color is black',
+    initialColor: 0,
+    paintedPanels: 2418
+  },
+  {
+    title: 'start color is white',
+    initialColor: 1,
+    paintedPanels: 249
+  },
+]
+
 if (require.main === module) {
   (async () => {
     console.log(chalk.yellowBright('How many panels does it paint at least once?'));
-    
+
     try {
+      const config = await getConfig();
       const program = await loadIntcodeFile(filename);
 
-      console.log('Painting...');
-      let i = 0;
-      const panels = await paintingRobot(program, onPaint);
-  
-      let paintedPanels = [...panels.values()].filter(panel => panel.coats > 0);
-  
-      console.log('Panels Painted', paintedPanels.length, (paintedPanels.length === 2418) ? '🏆' : '❌');
+      await runPaintingRobotWithConfig(program.slice(), config);
+      console.log('');
 
-      renderPanels(process.stdout, panels);
     } catch (ex) {
       console.error(ex);
     }
 
   })();
+}
+
+async function runPaintingRobotWithConfig(program, config) {
+  console.log('Painting...');
+  const panels = initPanels();
+  const panel0 = panels.getPanel([0, 0]);
+  panel0.color = config.initialColor;
+
+  for await (const args of paintingRobotEventGenerator(panels, program)) {
+    if (typeof onPaint === "function")
+      onPaint.apply(panels, args);
+  }
+
+  renderAllPanels(process.stdout, panels);
+  let paintedPanels = [...panels.values()].filter(panel => panel.coats > 0);
+
+  console.log('Panels Painted', paintedPanels.length, (paintedPanels.length === config.paintedPanels) ? '🏆' : '❌');
 }
 
 function onPaint(panel, state) {
@@ -36,5 +61,32 @@ function onPaint(panel, state) {
   console.log("Painting", colorFn(panel.color), ['^', '>', 'v', '<'][state.dir], state.coord, '              ');
 }
 
+async function getConfig() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  return new Promise((resolve, reject) => {
+    // Print options
+    configs.forEach((config, index) => {
+      console.log(index + 1, config.title);
+    });
+
+    rl.question('Select: ', (inputString) => {
+      rl.close();
+
+      let input = parseInt(inputString, 10);
+      let config = configs[input -1];
+      if (config) {
+        readline.moveCursor(rl.input, 0, -1 * configs.length - 1);
+        readline.clearScreenDown(rl.input);
+        console.log(chalk.green(config.title));
+        resolve(config);
+      }
+      reject(`Config ${inputString} not found`);
+    });
+  });
+}
 module.exports = {
 };
--- day11/render.js
+++ day11/render.js
@@ -1,70 +1,84 @@
 
-function renderPanels(writeStream, panels) {
+function renderAllPanels(writeStream, panels) {
   // Get extents
-  let xMin = 0, xMax = 0, yMin = 0, yMax = 0;
-  for (panel of panels.values()) {
-    xMin = Math.min(xMin, panel.coord[0]);
-    xMax = Math.max(xMax, panel.coord[0]);
-    yMin = Math.min(yMin, panel.coord[1]);
-    yMax = Math.max(yMax, panel.coord[1]);
-  }
+  let { plotWidth, plotHeight, offsetX, offsetY } = calculatePlotExtents(panels, writeStream);
 
-  let width = xMax - xMin;
-  let height = yMax - yMin;
-  if (writeStream.columns < (width / 2) || writeStream.rows < (height / 2)) {
-    console.log('Not enough room to render', 
-      `Need: ${width} x ${height}`, 
-      `Have: ${writeStream.columns} x ${writeStream.rows}`);
-    console.log(width, height, xMin, xMax, yMin, yMax);
-  }
+  const cursor = preparePlotArea(writeStream, plotWidth, plotHeight);
+  
+  plotPanels(offsetX, offsetY, panels, cursor);
 
-  console.log('Panel area', width, 'x', height);
-
-  let plotWidth = Math.ceil((width+1) / 2);
-  let plotHeight = Math.ceil((height+1) / 2);
-  const cursor = createPlotArea(writeStream, plotWidth, plotHeight);
-
-  const blocks = new Map();
-  for (panel of panels.values()) {
-    plotPanelRelative(panel, cursor);
-  }
-
-  cursor.moveTo(0, plotHeight);
-  console.log('')
+  cursor.close('Extents', plotWidth, plotHeight, offsetX, offsetY);
   return;
 
-  function createPlotArea(writeStream, width, height) {
-    const boxLines = ['┃', '━', '┏', '┓', '┗', '┛'];
-    console.log('┏' + ''.padEnd(width, '━') + '┓');
-    for (let i = height; i--; ) {
-      console.log('┃' + ''.padEnd(width, ' ') + '┃');
-    }
-    console.log('┗' + ''.padEnd(width, '━') + '┛');
-    writeStream.moveCursor(1, -(height+1));
+}
 
-    let cursor = { 
-      x: 0, 
-      y: 0,
-      moveTo: (x, y) => {
-        // writeStream.cursorTo(x, y);
-        let dx = x - cursor.x;
-        let dy = y - cursor.y;
-        writeStream.moveCursor(dx, dy);
-        cursor.x = x;
-        cursor.y = y;
-      },
-      write: (chars) => {
-        writeStream.write(chars);
-        cursor.x += chars.length;
-      }
-    };
-    
-    return cursor;
+function calculatePlotExtents(panels, writeStream) {
+  let offsetX = 0, xMax = 0, offsetY = 0, yMax = 0;
+  for (panel of panels.values()) {
+    offsetX = Math.min(offsetX, panel.coord[0]);
+    xMax = Math.max(xMax, panel.coord[0]);
+    offsetY = Math.min(offsetY, panel.coord[1]);
+    yMax = Math.max(yMax, panel.coord[1]);
+  }
+  let width = xMax - offsetX;
+  let height = yMax - offsetY;
+  if (writeStream.columns < (width / 2) || writeStream.rows < (height / 2)) {
+    console.log('Not enough room to render', `Need: ${width} x ${height}`, `Have: ${writeStream.columns} x ${writeStream.rows}`);
+    console.log(width, height, offsetX, xMax, offsetY, yMax);
+  }
+  let plotWidth = Math.ceil((width + 1) / 2);
+  let plotHeight = Math.ceil((height + 1) / 2);
+  return { plotWidth, plotHeight, offsetX, offsetY };
+}
+
+function preparePlotArea(writeStream, width, height) {
+  const boxLines = ['┃', '━', '┏', '┓', '┗', '┛'];
+  console.log('┏' + ''.padEnd(width, '━') + '┓');
+  for (let i = height; i--;) {
+    console.log('┃' + ''.padEnd(width, ' ') + '┃');
+  }
+  console.log('┗' + ''.padEnd(width, '━') + '┛');
+  // writeStream.moveCursor(1, -(height + 1));
+  writeStream.moveCursor(1, -2);
+
+  let cursor = {
+    x: 0,
+    y: 0,
+    moveTo: (x, y) => {
+      let dx = x - cursor.x;
+      let dy = y - cursor.y;
+      writeStream.moveCursor(dx, -dy);
+      cursor.x = x;
+      cursor.y = y;
+    },
+    write: (chars) => {
+      writeStream.write(chars);
+      cursor.x += chars.length;
+    },
+    close: (...args) => {
+      // cursor.moveTo(0, height);
+      cursor.moveTo(0, -1);
+      console.log.apply(console, args);
+    }
+  };
+
+  return cursor;
+}
+
+function plotPanels(xMin, yMin, panels, cursor) {
+  const blocks = new Map();
+  const offset = {
+    x: xMin * -1,
+    y: yMin * -1
+  };
+  for (panel of panels.values()) {
+    plotPanelRelative(panel, cursor, offset);
   }
 
-  function plotPanelRelative(panel, cursor) {
-    const plotCoordX = Math.floor((panel.coord[0] - xMin) / 2);
-    const plotCoordY = Math.floor((panel.coord[1] - yMin) / 2);
+
+  function plotPanelRelative(panel, cursor, offset) {
+    const plotCoordX = Math.floor((panel.coord[0] + offset.x) / 2);
+    const plotCoordY = Math.floor((panel.coord[1] + offset.y) / 2);
     const blockCoordX = Math.abs(panel.coord[0]) % 2;
     const blockCoordY = Math.abs(panel.coord[1]) % 2;
     const blockKey = `${plotCoordX},${plotCoordY}`;
@@ -77,15 +91,12 @@ function renderPanels(writeStream, panels) {
       blocks.set(blockKey, block);
     }
 
+    // Set block data to panel color
     block[blockCoordY][blockCoordX] = panel.color;
 
+    // Re-render this block.
     cursor.moveTo(plotCoordX, plotCoordY);
-    // writeStream.moveCursor(plotCoordX, plotCoordY);
     cursor.write(getBlockChar(block));
-    // writeStream.moveCursor(-plotCoordX, -plotCoordY);
-
-    // cursor.x = blockCoordX;
-    // cursor.y = blockCoordX;
   }
 
 }
@@ -96,9 +107,9 @@ function getBlockChar(block) {
   // U+258x	▀	▁	▂	▃	▄	▅	▆	▇	█	▉	▊	▋	▌	▍	▎	▏
   // U+259x	▐	░	▒	▓	▔	▕	▖	▗	▘	▙	▚	▛	▜	▝	▞	▟
   const blockChars = [
-    ' ', '▝', '▘', '▀', 
-    '▗', '▐', '▚', '▜', 
-    '▖', '▞', '▌', '▛', 
+    ' ', '▝', '▘', '▀',
+    '▗', '▐', '▚', '▜',
+    '▖', '▞', '▌', '▛',
     '▄', '▟', '▙', '█'
   ];
 
@@ -118,4 +129,4 @@ function getBlockChar(block) {
   return blockChars[index];
 }
 
-module.exports = { renderPanels };
+module.exports = { renderAllPanels, calculatePlotExtents, preparePlotArea, plotPanels };
